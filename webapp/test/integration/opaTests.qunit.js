44f3acb9a1d6b453142824e35529608bd0c2b886
Marian Abergale Tan <marian.abergale.tan@accenture.com>
2023-03-22 09:51:18 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zbtp/navigation/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zbtp/navigation/test/unit/controller/Overview.controller"
+], function () {
+	"use strict";
+});
